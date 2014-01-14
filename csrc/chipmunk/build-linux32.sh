61764d81de8edcabc21b8b6d72cfe3e36e53c151
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-14 14:41:15 +0200

    added $CFLAGS option and recompiled with -O2

--- csrc/chipmunk/build-linux32.sh
+++ csrc/chipmunk/build-linux32.sh
@@ -1,4 +1,3 @@
-gcc src/*.c src/constraints/*.c \
-	-shared -O3 -s -o ../../bin/linux32/libchipmunk.so -Iinclude/chipmunk \
+gcc $CFLAGS src/*.c src/constraints/*.c \
+	-shared -o ../../bin/linux32/libchipmunk.so -Iinclude/chipmunk \
 	-std=gnu99 -Wall -ffast-math -DNDEBUG -DCHIPMUNK_FFI \
-
--- csrc/chipmunk/build-mingw32.sh
+++ csrc/chipmunk/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc src/*.c src/constraints/*.c \
-	-shared -O3 -s -o ../../bin/mingw32/chipmunk.dll -Iinclude/chipmunk \
+gcc $CFLAGS src/*.c src/constraints/*.c \
+	-shared -o ../../bin/mingw32/chipmunk.dll -Iinclude/chipmunk \
 	-std=gnu99 -Wall -ffast-math -DNDEBUG -DCHIPMUNK_FFI \
