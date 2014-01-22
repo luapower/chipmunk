ac68cd16861d4aae06415999564649128ed08ed2
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:50:02 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/chipmunk/build-linux32.sh
+++ csrc/chipmunk/build-linux32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS src/*.c src/constraints/*.c \
+gcc -O2 -s -static-libgcc src/*.c src/constraints/*.c \
 	-shared -o ../../bin/linux32/libchipmunk.so -Iinclude/chipmunk \
 	-std=gnu99 -Wall -ffast-math -DNDEBUG -DCHIPMUNK_FFI \
--- csrc/chipmunk/build-mingw32.sh
+++ csrc/chipmunk/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS src/*.c src/constraints/*.c \
+gcc -O2 -s -static-libgcc src/*.c src/constraints/*.c \
 	-shared -o ../../bin/mingw32/chipmunk.dll -Iinclude/chipmunk \
 	-std=gnu99 -Wall -ffast-math -DNDEBUG -DCHIPMUNK_FFI \
