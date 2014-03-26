9381723cbc4bb72b61bd1c3726fc14af62d059e7
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:06:23 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/chipmunk/build-osx32.sh
+++ csrc/chipmunk/build-osx32.sh
@@ -1,2 +1,2 @@
-gcc -arch i386 -O2 src/*.c src/constraints/*.c -shared -o ../../bin/osx32/libchipmunk.dylib -Iinclude/chipmunk \
+gcc -arch i386 -O2 src/*.c src/constraints/*.c -shared -install_name @loader_path/libchipmunk.dylib -o ../../bin/osx32/libchipmunk.dylib -Iinclude/chipmunk \
 	-std=gnu99 -Wall -ffast-math -DNDEBUG -DCHIPMUNK_FFI
--- csrc/chipmunk/build-osx64.sh
+++ csrc/chipmunk/build-osx64.sh
@@ -1,2 +1,2 @@
-gcc -arch x86_64 -O2 src/*.c src/constraints/*.c -shared -o ../../bin/osx64/libchipmunk.dylib -Iinclude/chipmunk \
+gcc -arch x86_64 -O2 src/*.c src/constraints/*.c -shared -install_name @loader_path/libchipmunk.dylib -o ../../bin/osx64/libchipmunk.dylib -Iinclude/chipmunk \
 	-std=gnu99 -Wall -ffast-math -DNDEBUG -DCHIPMUNK_FFI
