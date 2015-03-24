1f744836f55748e039cc0d9c6bd219478737f6e5
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 12:58:13 +0200

    unimportant

--- csrc/chipmunk/build-linux64.sh
+++ csrc/chipmunk/build-linux64.sh
@@ -1,1 +1,1 @@
-P=linux64 C=-fPIC L="-s -static-libgcc" D=libchipmunk.so A=libchipmunk.a ./build.sh
+P=linux64 C="-fPIC -include _memcpy.h" L="-s -static-libgcc" D=libchipmunk.so A=libchipmunk.a ./build.sh
